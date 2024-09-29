e590f0f930c03604c761a5faae2c02ebeb58d40d
LEEYONGJAE <ionsi1103@gmail.com>
2024-09-29 14:30:41 +0900

    [new] Trash flight
    in Youtube cloned.

---
+++ Assets/Scripts/Background.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Scripting.APIUpdating;
+
+public class Background : MonoBehaviour
+{
+    private float moveSpeed = 3f;
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position += Vector3.down * moveSpeed * Time.deltaTime;
+        
+        if (transform.position.y < -10f)
+        {
+            transform.position += new Vector3(0f, 20f, 0f);
+        }
+    }
+}
---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,66 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UIElements;
+
+public class PlayerMovement : MonoBehaviour
+{
+    [SerializeField]
+    private float moveSpeed;
+
+    [SerializeField]
+    private GameObject weapon;
+
+    [SerializeField]
+    private Transform shootTransform;
+
+    [SerializeField]
+    private float shootInterval = 0.05f;
+    private float lastShotTime = 0f;
+
+    void Shoot()
+    {
+        if (Time.time - lastShotTime > shootInterval)
+        {
+            Instantiate(weapon, shootTransform.position, Quaternion.identity);
+            lastShotTime = Time.time;
+        }
+        
+    }
+    void Update()
+    {
+        // float horizontalInput = Input.GetAxisRaw("Horizontal");
+        // float verticalInput = Input.GetAxisRaw("Vertical");
+
+        // Vector3 moveTo = new Vector3(horizontalInput, verticalInput, 0f);
+
+        // transform.position += moveTo * moveSpeed * Time.deltaTime;
+
+
+        // Vector3 moveTo = new Vector3(moveSpeed * Time.deltaTime, 0f, 0f);
+        // if (Input.GetKey(KeyCode.LeftArrow))
+        // {
+        //     transform.position -= moveTo;
+        // }
+        // else if (Input.GetKey(KeyCode.RightArrow))
+        // {
+        //     transform.position += moveTo;
+        // }
+        
+        // Debug.Log(Input.mousePosition);
+
+        Vector3 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        // Debug.Log(mousePos);
+
+        float toX = Mathf.Clamp(mousePos.x, -2.4f, 2.4f);
+        transform.position = new Vector3(
+            toX, transform.position.y, transform.position.z);
+
+        
+        Shoot();
+    }
+
+    
+
+}
